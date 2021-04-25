35453d80638af0041d0fc12c249cdc06756396a8
Sweny <sweny.bagrecha@twobulls.com>
2021-04-25 13:31:52 +1000

    Display list of only the cats, grouped by their owner's gender and then sorted in alphabetical order.

--- app/src/main/java/au/com/agl/kotlincats/domain/MainUseCases.kt
+++ app/src/main/java/au/com/agl/kotlincats/domain/MainUseCases.kt
@@ -5,11 +5,22 @@ import au.com.agl.kotlincats.data.OwnerRepository
 import au.com.agl.kotlincats.data.model.Owner
 
 class MainUseCases(private val ownerRepository: OwnerRepository): MainFacade {
-    override fun loadGroupedCats(callback: Callback<Any>) {
+    override fun loadGroupedCats(callback: Callback<List<PetOwnerData>>) {
         ownerRepository.get(object: Callback<List<Owner>> {
             override fun onSuccess(data: List<Owner>) {
                 // TODO: manipulate data as appropriate and feed into callback
-                callback.onSuccess(data)
+                //Manipulated JSON data to look particularly for pet type : Cat
+                //Create a list containing respective cat(s) owner's gender
+                val list: MutableList<PetOwnerData> = mutableListOf()
+                data.forEach { owners ->
+                    owners.pets?.forEach { pets ->
+                        if (pets.type == "Cat") {
+                            val manipulatedList = PetOwnerData(owners.gender, pets.name)
+                            list.add(manipulatedList)
+                        }
+                    }
+                }
+                callback.onSuccess(list)
             }
 
             override fun onError(error: Throwable) {
@@ -18,3 +29,6 @@ class MainUseCases(private val ownerRepository: OwnerRepository): MainFacade {
         })
     }
 }
+
+data class PetOwnerData(val gender:String, val petName: String)
+data class SortedPetOwnerData(val genGroup:String, val petGroup: String)
